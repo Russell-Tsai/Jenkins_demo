3a1c3550a85ba0aa17f13ad7a968766849e99812
Russell <vanilla.love10@gmail.com>
2019-05-25 13:26:43 +0800

    Initial Commit

---
+++ Jenkins_demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jenkins_demo
+//
+//  Created by Vanilla on 2019/5/25.
+//  Copyright © 2019 Vanilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
